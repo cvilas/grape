01e12a5293cfdabff7dd9761c8aacdef453d633b
Vilas <cvilas@gmail.com>
2023-09-17 18:29:03 +0100

    feat: Introduces logging module
    (cherry picked from commit 68492536fad98f043712a7f20f471659293c1822)

---
+++ modules/common/log/examples/custom_sink_example.cpp
@@ -0,0 +1,37 @@
+//=================================================================================================
+// Copyright (C) 2018-2023 GRAPE Contributors
+//=================================================================================================
+
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+
+#include "grape/log/log.h"
+
+//=================================================================================================
+// Demonstrates how to redirect the default console output to a different stream (file, string, etc)
+auto main(int argc, const char* argv[]) -> int {
+  (void)argc;
+  (void)argv;
+
+  // sends message to default stream buffer, which is the console
+  grape::log::log(grape::log::Severity::Info, "Message to default output stream");
+
+  // save pointer to current output stream buffer and substitute it with a custom stream buffer.
+  std::ostringstream custom_stream;
+  auto* previous_stream = grape::log::setStreamBuffer(custom_stream.rdbuf());
+
+  // now logger works with custom buffer. you don't see this message on the console.
+  grape::log::log(grape::log::Severity::Info, "Message to custom output stream");
+
+  // go back to old buffer
+  grape::log::setStreamBuffer(previous_stream);
+
+  // you will see this message
+  grape::log::log(grape::log::Severity::Info, "Message directed *back* to default output stream");
+
+  // print content from custom stream
+  std::cout << "Custom stream content:\n" << custom_stream.str() << "\n";
+
+  return EXIT_SUCCESS;
+}
---
+++ modules/common/log/tests/tests.cpp
@@ -0,0 +1,17 @@
+//=================================================================================================
+// Copyright (C) 2018-2023 GRAPE Contributors
+//=================================================================================================
+
+#include "catch2/catch_test_macros.hpp"
+
+namespace grape::log::tests {
+
+// NOLINTBEGIN(cert-err58-cpp)
+
+TEST_CASE("[example test]", "[log]") {
+  CHECK(false);
+}
+
+// NOLINTEND(cert-err58-cpp)
+
+}  // namespace grape::log::tests
