2c8c988aa58d2c9a7d482e1ca16e5d3e0a2a40a1
Vilas Chitrakaran <cvilas@gmail.com>
2024-01-15 11:25:50 +0000

    refactor: Updates logger example
    (cherry picked from commit 12de2f4e459f1ce24df9f32b76be6ca9e16a771c)

--- modules/common/log/examples/example.cpp
+++ modules/common/log/examples/example.cpp
@@ -2,15 +2,21 @@
 // Copyright (C) 2018-2023 GRAPE Contributors
 //=================================================================================================
 
-#include <cstdlib>
-
 #include "grape/log/logger.h"
 
 //=================================================================================================
 auto main(int argc, const char* argv[]) -> int {
   (void)argc;
   (void)argv;
-  grape::log::Logger logger({});
-  logger.log(grape::log::Severity::Error, "A log message");
-  return EXIT_SUCCESS;
+  try {
+    auto config = grape::log::Config();
+    config.logger_name = "logger example";
+    config.threshold = grape::log::Severity::Debug;
+    auto logger = grape::log::Logger(std::move(config));
+    logger.log(grape::log::Severity::Error, "A log message");
+    return EXIT_SUCCESS;
+  } catch (const std::exception& ex) {
+    std::ignore = fputs(ex.what(), stderr);
+    return EXIT_FAILURE;
+  }
 }
