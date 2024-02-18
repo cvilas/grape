821b2a667f277d38a181767b1adff942e1863e2b
Vilas <cvilas@gmail.com>
2024-02-18 20:23:25 +0000

    chore: Cleans up some documentation
    (cherry picked from commit bfcb5eeb8a5bc66f6090177b0b7ff0cb6ee9a04a)

--- modules/common/base/include/grape/exception.h
+++ modules/common/base/include/grape/exception.h
@@ -40,10 +40,10 @@ public:
 /// - Use exceptions only for serious/infrequent/unexpected errors where getting out of control
 /// flow to prevent further damage is more important than trying to continue onward.
 ///   - For shallow returns, prefer 'std::optional' and 'std::expected'
-///   - Define as few exception types as possible
-///   - Exceptions are defined by their catch handler usage. Use them for:
-///     - Error tracing and logging
-///     - Stack unwinding for terminally fatal or transactionally fatal errors
+/// - Define as few exception types as possible
+/// - Exceptions are defined by their catch handler usage. Use them for:
+///   - Error tracing and logging
+///   - Stack unwinding for terminally fatal or transactionally fatal errors
 /// - The following NOT good uses for exceptions:
 ///   - Resource management (eg: to release resources in catch block). Use RAII instead.
 ///   - Loop control flow. Use return status codes instead.
