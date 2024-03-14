3ef70c202954b22d030be79f2fb76abb61927fbe
Vilas <cvilas@gmail.com>
2024-03-14 06:24:10 -0400

    refactor: Implements logger benchmarking using Google benchmark
    (cherry picked from commit a0052a2198c467a23f22bf8567d1762b2b2ffc3b)

--- modules/common/log/examples/spdlog_bench.cpp
+++ modules/common/log/examples/spdlog_bench.cpp
@@ -4,28 +4,28 @@
 
 #include <fstream>
 
+#include <benchmark/benchmark.h>
+
 #include "grape/log/logger.h"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/spdlog.h"
 
 //-------------------------------------------------------------------------------------------------
-auto benchmarkSpdlog(std::size_t num_iterations) -> std::chrono::microseconds {
+auto bmSpdlog(benchmark::State& state) {
   auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("benchmark_spdlog_out.txt", true);
   auto logger = std::make_shared<spdlog::logger>("benchmark_spdlogger", sink);
-
-  const auto start_time = std::chrono::high_resolution_clock::now();
-  for (std::size_t i = 0; i < num_iterations; ++i) {
-    SPDLOG_LOGGER_INFO(logger, "Log message {:d}", i);
+  auto i = 0uz;
+  for (auto s : state) {
+    (void)s;
+    SPDLOG_LOGGER_INFO(logger, "Log message {:d}", i++);
   }
-  const auto end_time = std::chrono::high_resolution_clock::now();
-  return std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
 }
 
 //-------------------------------------------------------------------------------------------------
-auto benchmarkGrapeLog(std::size_t num_iterations) -> std::chrono::microseconds {
+auto bmGrapeLog(benchmark::State& state) {
   auto log_file = std::ofstream("benchmark_grapelog_out.txt");
   auto config = grape::log::Config();
-  config.queue_capacity = num_iterations;
+  config.queue_capacity = static_cast<std::size_t>(state.max_iterations);
   config.flush_period = std::chrono::microseconds(100);
   config.sink = [&log_file](const grape::log::Record& r) {
     log_file << grape::log::defaultFormatter(r) << '\n';
@@ -33,25 +33,23 @@ auto benchmarkGrapeLog(std::size_t num_iterations) -> std::chrono::microseconds 
   config.logger_name = "benchmark_grapelog";
 
   auto logger = std::make_unique<grape::log::Logger>(std::move(config));
-  const auto start_time = std::chrono::high_resolution_clock::now();
-  for (std::size_t i = 0; i < num_iterations; ++i) {
-    logger->log(grape::log::Severity::Info, std::format("Log message {:d}", i));
+  auto i = 0uz;
+  for (auto s : state) {
+    (void)s;
+    logger->log(grape::log::Severity::Info, std::format("Log message {:d}", i++));
   }
-  const auto end_time = std::chrono::high_resolution_clock::now();
-  return std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
 }
 
 //=================================================================================================
 // Benchmark our logger against spdlog (https://github.com/gabime/spdlog)
-auto main() -> int {
-  constexpr std::size_t NUM_ITERATIONS = 100000;  // Number of log messages to output
 
-  const auto spdlog = benchmarkSpdlog(NUM_ITERATIONS);
-  const auto log = benchmarkGrapeLog(NUM_ITERATIONS);
+constexpr auto MAX_ITERATIONS = 1000000U;
 
-  std::println("Benchmark {} iterations:", NUM_ITERATIONS);
-  std::println("  spdlog {} us", spdlog.count());
-  std::println("  grape  {} us", log.count());
+// NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cert-err58-cpp,cppcoreguidelines-avoid-non-const-global-variables)
+BENCHMARK(bmSpdlog)->Iterations(MAX_ITERATIONS);
 
-  return EXIT_SUCCESS;
-}
+// NOLINTNEXTLINE(cppcoreguidelines-owning-memory,cert-err58-cpp,cppcoreguidelines-avoid-non-const-global-variables)
+BENCHMARK(bmGrapeLog)->Iterations(MAX_ITERATIONS);
+
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays,cppcoreguidelines-pro-bounds-array-to-pointer-decay,modernize-use-trailing-return-type)
+BENCHMARK_MAIN();
