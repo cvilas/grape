2028c32cbf29c66b3cef5df952b5c889048f125c
Vilas Chitrakaran <cvilas@gmail.com>
2024-04-05 07:50:57 +0100

    build: Turns off warnings for C-style casts
    This breaks compilation when using certain third-party C libraries (eg: SDL) that use such casts in their headers
    
    (cherry picked from commit f8cbb2541ddfe20076eafbbbb52dee747f24ff69)

--- gbs/03_compiler_config.cmake
+++ gbs/03_compiler_config.cmake
@@ -15,11 +15,10 @@ set(THIRD_PARTY_COMPILER_WARNINGS -Wall -Wextra -Wpedantic)
 set(CLANG_WARNINGS
     -Wshadow # warn the user if a variable declaration shadows one from a parent context
     -Wnon-virtual-dtor # warn if a class with virtual functions has a non-virtual destructor.
-    -Wold-style-cast # warn for c-style casts
+    #-Wold-style-cast # warn for c-style casts
     -Wcast-align # warn for potential performance problem casts
     -Wunused # warn on anything being unused
     -Woverloaded-virtual # warn if you overload (not override) a virtual function
-    -Wpedantic # warn if non-standard C++ is used
     -Wconversion # warn on type conversions that may lose data
     -Wsign-conversion # warn on sign conversions
     -Wnull-dereference # warn if a null dereference is detected
