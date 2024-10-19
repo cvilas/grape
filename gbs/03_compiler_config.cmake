86a11206076be2df0b35cc719be5ea3f4c94864f
Vilas Chitrakaran <cvilas@gmail.com>
2024-10-19 18:55:20 +0100

    build: Turns on all clang compiler warnings
    (cherry picked from commit 8e740964be069a891f9b3c917978a2e5dc3beb0e)

--- gbs/03_compiler_config.cmake
+++ gbs/03_compiler_config.cmake
@@ -12,29 +12,43 @@ add_compile_options(-Wall -Wextra -Wpedantic -Werror)
 set(THIRD_PARTY_COMPILER_WARNINGS -Wall -Wextra -Wpedantic)
 
 # clang warnings
-set(CLANG_WARNINGS
-    -Wshadow # warn the user if a variable declaration shadows one from a parent context
-    -Wnon-virtual-dtor # warn if a class with virtual functions has a non-virtual destructor.
-    #-Wold-style-cast # warn for c-style casts
-    -Wcast-align # warn for potential performance problem casts
-    -Wunused # warn on anything being unused
-    -Woverloaded-virtual # warn if you overload (not override) a virtual function
-    -Wconversion # warn on type conversions that may lose data
-    -Wsign-conversion # warn on sign conversions
-    -Wnull-dereference # warn if a null dereference is detected
-    -Wdouble-promotion # warn if float is implicit promoted to double
-    -Wformat=2 # warn on security issues around functions that format output (ie printf)
-    -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation
-)
+set(CLANG_WARNINGS -Weverything 
+  -Wno-c++20-compat 
+  -Wno-pre-c++20-compat-pedantic 
+  -Wno-pre-c++17-compat 
+  -Wno-c++98-compat 
+  -Wno-c++98-compat-pedantic 
+  -Wno-unsafe-buffer-usage 
+  -Wno-padded 
+  -Wno-switch-default 
+  -Wno-ctad-maybe-unsupported
+  -Wno-global-constructors
+  -Wno-weak-vtables
+  -Wno-exit-time-destructors)
 
 # GCC warnings
 set(GCC_WARNINGS
-    ${CLANG_WARNINGS}
-    -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
-    -Wduplicated-cond # warn if if / else chain has duplicated conditions
-    -Wduplicated-branches # warn if if / else branches have duplicated code
-    -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
-    -Wuseless-cast # warn if you perform a cast to the same type
+  -Wshadow # warn the user if a variable declaration shadows one from a parent context
+  -Wnon-virtual-dtor # warn if a class with virtual functions has a non-virtual destructor.
+  -Wold-style-cast # warn for c-style casts
+  -Wcast-align # warn for potential performance problem casts
+  -Wunused # warn on anything being unused
+  -Woverloaded-virtual # warn if you overload (not override) a virtual function
+  -Wconversion # warn on type conversions that may lose data
+  -Wsign-conversion # warn on sign conversions
+  -Wnull-dereference # warn if a null dereference is detected
+  -Wdouble-promotion # warn if float is implicit promoted to double
+  -Wformat=2 # warn on security issues around functions that format output (ie printf)
+  -Wimplicit-fallthrough # warn on statements that fallthrough without an explicit annotation    
+  -Wmisleading-indentation # warn if indentation implies blocks where blocks do not exist
+  -Wduplicated-cond # warn if if / else chain has duplicated conditions
+  -Wduplicated-branches # warn if if / else branches have duplicated code
+  -Wlogical-op # warn about logical operations being used where bitwise were probably wanted
+  -Wuseless-cast # warn if you perform a cast to the same type
+  -Wconversion 
+  -Wcast-qual 
+  -Wpointer-arith 
+  -Wstrict-prototypes
 )
 
 if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
