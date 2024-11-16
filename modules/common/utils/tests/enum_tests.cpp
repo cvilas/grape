90788ab7d85361e75b08a2ef30871e3dd811a82e
Vilas Chitrakaran <cvilas@gmail.com>
2024-11-16 19:21:49 +0000

    feat: Adds a function to get enumeration from string
    (cherry picked from commit 997d184301ef0809cb167f412bf86f101e4f3379)

--- modules/common/utils/tests/enum_tests.cpp
+++ modules/common/utils/tests/enum_tests.cpp
@@ -37,5 +37,30 @@ TEST_CASE("Invalid values within enum range return empty string", "[enums]") {
   REQUIRE(grape::enums::enum_name(static_cast<Color>(-1)).empty());
   REQUIRE(grape::enums::enum_name(static_cast<Color>(3)).empty());
 }
+
+//-------------------------------------------------------------------------------------------------
+TEST_CASE("Valid string names can be cast back to enum", "[enums]") {
+  auto maybe_color = grape::enums::enum_cast<Color>("Red");
+  REQUIRE((maybe_color.has_value() and (maybe_color.value() == Color::Red)));
+
+  maybe_color = grape::enums::enum_cast<Color>("Green");
+  REQUIRE((maybe_color.has_value() and (maybe_color.value() == Color::Green)));
+
+  maybe_color = grape::enums::enum_cast<Color>("Blue");
+  REQUIRE((maybe_color.has_value() and (maybe_color.value() == Color::Blue)));
+
+  maybe_color = grape::enums::enum_cast<Color>("Black");
+  REQUIRE((maybe_color.has_value() and (maybe_color.value() == Color::Black)));
+
+  maybe_color = grape::enums::enum_cast<Color>("White");
+  REQUIRE((maybe_color.has_value() and (maybe_color.value() == Color::White)));
+}
+
+//-------------------------------------------------------------------------------------------------
+TEST_CASE("Invalid string name can be cast back to enum", "[enums]") {
+  const auto maybe_color = grape::enums::enum_cast<Color>("Red--");
+  REQUIRE(not maybe_color.has_value());
+}
+
 }  // namespace
 // NOLINTEND(cert-err58-cpp)
