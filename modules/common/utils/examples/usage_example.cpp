655091cc33088153d7bd7cf6f9f3a872fe4c12d3
Vilas Chitrakaran <cvilas@gmail.com>
2024-11-17 20:32:22 +0000

    feat: Implements function to return search path for application data files

--- modules/common/utils/examples/usage_example.cpp
+++ modules/common/utils/examples/usage_example.cpp
@@ -11,7 +11,9 @@ auto main() -> int {
   try {
     const auto path = grape::utils::getProgramPath();
     const auto host_name = grape::utils::getHostName();
+    const auto home_dir = grape::utils::getUserHomePath();
     std::println("Host name: {}", host_name);
+    std::println("User's home: {}", home_dir.string());
     std::println("Program name: {}", path.filename().string());
     std::println("Program path: {}", path.parent_path().string());
     return EXIT_SUCCESS;
