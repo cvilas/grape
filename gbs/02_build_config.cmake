625070c87bd5daa5146b8c63c4d23560d6b8374a
Vilas Chitrakaran <cvilas@gmail.com>
2025-01-11 09:58:32 +0000

    build: Force CMAKE_INSTALL_LIBDIR
    (cherry picked from commit 6fcf6446515e46ff03c66803174e9ff6e304feba)

--- gbs/02_build_config.cmake
+++ gbs/02_build_config.cmake
@@ -32,6 +32,7 @@ endif()
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
+set(CMAKE_INSTALL_LIBDIR lib) # override default lib64 on some systems
 
 include(CMakePackageConfigHelpers)
 include(GNUInstallDirs)
