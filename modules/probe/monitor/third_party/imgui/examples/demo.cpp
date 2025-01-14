2aa12dfca5fa76615972e265bc14aba2f1b862ab
Vilas Chitrakaran <cvilas@gmail.com>
2025-01-14 20:29:53 +0000

    Fix: imgui demo
    (cherry picked from commit db071df76fa39b77556ff34b005700d423200cd9)

--- modules/probe/monitor/third_party/imgui/examples/demo.cpp
+++ modules/probe/monitor/third_party/imgui/examples/demo.cpp
@@ -8,14 +8,14 @@
 // - Introduction, links and more at the top of imgui.cpp
 
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#include <stdio.h>
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
 #endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include <cstdio>
 
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 // To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
