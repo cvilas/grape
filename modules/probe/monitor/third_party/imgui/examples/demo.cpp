2d3bc6e541258be1d7be7c1f350aa894ea2bfa80
Vilas Chitrakaran <cvilas@gmail.com>
2025-03-14 17:00:57 +0000

    feat: Updates imgui and implot
    (cherry picked from commit 2ba7707fa7581892ecb7919729a1659a121f0d64)

--- modules/probe/monitor/third_party/imgui/examples/demo.cpp
+++ modules/probe/monitor/third_party/imgui/examples/demo.cpp
@@ -10,12 +10,12 @@
 #include "imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
+#include <stdio.h>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
 #endif
 #include <GLFW/glfw3.h> // Will drag system OpenGL headers
-#include <cstdio>
 
 // [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
 // To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
