a5e05e1d072b3567aa8e83e60a8a86a3122fa07c
Vilas Chitrakaran <cvilas@gmail.com>
2025-04-05 20:18:02 +0100

    refactor(utils): Refactors enum reflection implementation details to a different file

--- modules/common/utils/include/grape/utils/enums.h
+++ modules/common/utils/include/grape/utils/enums.h
@@ -13,71 +13,12 @@
 
 #pragma once
 
-#include <array>
 #include <optional>
-#include <string_view>
 #include <type_traits>
-#include <utility>
+
+#include "grape/utils/detail/enums_detail.h"
 
 namespace grape::enums {
-namespace detail {
-
-/// Uses compiler-specific magic to extract a string representing an enumeration at compile-time
-template <auto E>
-consteval auto extractEnumeratorName() -> std::string_view {
-  const std::string_view func_name = __PRETTY_FUNCTION__;
-
-#if defined(__clang__)
-  const std::string_view start_token = "[E = ";
-  const std::string_view end_token = "]";
-#elif defined(__GNUC__) || defined(__GNUG__)
-  const std::string_view start_token = "with auto E = ";
-  const std::string_view end_token = ";";
-#else
-#error "Unsupported compiler. Use GCC or Clang"
-#endif
-  const auto temp_args_list = func_name.substr(func_name.find(start_token) + start_token.size());
-  if (temp_args_list.starts_with("(")) {
-    return "";
-  }
-
-  const auto enum_name_end = temp_args_list.find(end_token);
-  const auto enum_name_start = temp_args_list.rfind("::", enum_name_end) + 2;
-  const auto enum_name_length = enum_name_end - enum_name_start;
-  return temp_args_list.substr(enum_name_start, enum_name_length);
-}
-
-/// Wrapper that extracts enum names and puts them in static storage as uniquely named variables
-template <auto E>
-struct NameStorage {
-  static constexpr auto NAME = extractEnumeratorName<E>();
-
-  static constexpr auto makeArray() {
-    std::array<char, NAME.size()> result{};
-    for (std::size_t i = 0; i < NAME.size(); ++i) {
-      result.at(i) = NAME.at(i);
-    }
-    return result;
-  }
-
-  static constexpr auto VALUE = makeArray();
-};
-
-template <auto E>
-consteval auto extractAndAllocateEnumeratorName() -> std::string_view {
-  return { NameStorage<E>::VALUE.data(), NameStorage<E>::VALUE.size() };
-}
-
-template <int N, int... Seq>
-constexpr auto add(std::integer_sequence<int, Seq...> /*unused*/)
-    -> std::integer_sequence<int, N + Seq...> {
-  return {};
-}
-
-template <int Min, int Max>
-using make_integer_range = decltype(add<Min>(std::make_integer_sequence<int, Max - Min>()));
-
-}  // namespace detail
 
 //-------------------------------------------------------------------------------------------------
 /// Sets default range of values an enum can take. Users can override with a specialisation for the
