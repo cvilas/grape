02af7e2f89fe2a50b9d3beab3e7d6b24a7d46bf3
Vilas Chitrakaran <cvilas@gmail.com>
2025-05-17 21:45:15 +0100

    detect stdlib

--- gbs/00_utilities.cmake
+++ gbs/00_utilities.cmake
@@ -11,3 +11,40 @@ function(print_cmake_variables)
     message(STATUS "${var}=${${var}}")
   endforeach()
 endfunction()
+
+include(CheckCXXSourceCompiles)
+
+#==================================================================================================
+# Detect which standard library is in use
+# USING_LIBSTDCXX is set if libstdc++ is in use
+# USING_LIBCPP is set if libc++ is in use
+function(detect_cxx_stdlib)
+    # Save the current compiler flags
+    set(CMAKE_REQUIRED_FLAGS_SAVED ${CMAKE_REQUIRED_FLAGS})
+    
+    # Test for libstdc++
+    set(CMAKE_REQUIRED_FLAGS "${CMAKE_CXX_FLAGS}")
+    check_cxx_source_compiles("
+        #include <cstddef>
+        #if !defined(__GLIBCXX__)
+        #error Not using libstdc++
+        #endif
+        int main() { return 0; }
+    " USING_LIBSTDCXX)
+    
+    # Test for libc++
+    check_cxx_source_compiles("
+        #include <cstddef>
+        #if !defined(_LIBCPP_VERSION)
+        #error Not using libc++
+        #endif
+        int main() { return 0; }
+    " USING_LIBCPP)
+    
+    # Restore compiler flags
+    set(CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS_SAVED})
+    
+    # Set results in parent scope
+    set(USING_LIBSTDCXX ${USING_LIBSTDCXX} PARENT_SCOPE)
+    set(USING_LIBCPP ${USING_LIBCPP} PARENT_SCOPE)
+endfunction()
