a8ac500f74135e970e42cd8c711ed190d4c0d59d
Vilas <cvilas@gmail.com>
2025-05-20 07:02:22 +0100

    build: Fixes clang toolchain to work on MacOS
    (cherry picked from commit c6381a842397b578973da3a9f15dc8fd70dca3ff)

--- toolchains/toolchain_clang.cmake
+++ toolchains/toolchain_clang.cmake
@@ -7,3 +7,11 @@ set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_FLAGS "-stdlib=libc++")
 set(CMAKE_EXE_LINKER_FLAGS "-stdlib=libc++ -lc++abi")
 set(CMAKE_CROSSCOMPILING FALSE)
+
+# Additional configuration for MacOS
+# LLVM must be installed: `brew install llvm`
+# LLVM binaries should be in path: `export PATH="/opt/homebrew/opt/llvm/bin:$PATH"`
+if(CMAKE_SYSTEM_NAME MATCHES "Darwin")
+  execute_process(COMMAND xcrun --show-sdk-path OUTPUT_VARIABLE CMAKE_OSX_SYSROOT OUTPUT_STRIP_TRAILING_WHITESPACE)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -isysroot ${CMAKE_OSX_SYSROOT}")
+endif()
