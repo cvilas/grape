b4371810ab1f28de4db48c35197b3fdb7eb7cf21
Vilas Chitrakaran <cvilas@gmail.com>
2025-06-10 18:08:17 +0100

    ci: Reorganises CI infra files

--- runner/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Fix permissions for mounted work directory and tool cache
-chown -R runner:runner /home/runner/_work || true
-#chown -R runner:runner /home/runner/_tool || true
-
-exec "$@"
