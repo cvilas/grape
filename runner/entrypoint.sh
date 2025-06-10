c7c4ed47dfa43835a1b38323d65b7bf3493c32d5
Vilas Chitrakaran <cvilas@gmail.com>
2025-06-10 16:25:53 +0100

    wip

--- runner/entrypoint.sh
+++ runner/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 # Fix permissions for mounted work directory and tool cache
 chown -R runner:runner /home/runner/_work || true
-chown -R runner:runner /home/runner/_tool || true
+#chown -R runner:runner /home/runner/_tool || true
 
 exec "$@"
