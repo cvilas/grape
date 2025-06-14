bdd7fb96e622f75370dc4c16bb925b104d3bde10
Vilas Chitrakaran <cvilas@gmail.com>
2025-06-14 18:12:44 +0100

    ci: Adds a bunch of small fixes to the runner infra

--- toolchains/install_cmake.sh
+++ toolchains/install_cmake.sh
@@ -12,7 +12,7 @@ wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | 
 echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" | \
   sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 
-sudo apt update
+sudo apt-get update
 sudo rm /usr/share/keyrings/kitware-archive-keyring.gpg
-sudo apt install -y kitware-archive-keyring
-sudo apt install -y cmake cmake-format
+sudo apt-get install -y kitware-archive-keyring
+sudo apt-get install -y cmake cmake-format
