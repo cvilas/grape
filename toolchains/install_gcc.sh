925f336c6fd75e86e6e61358850da2f20fdedb1b
Vilas Chitrakaran <cvilas@gmail.com>
2025-06-15 08:12:23 +0100

    fix: Updates a few scripts

--- toolchains/install_gcc.sh
+++ toolchains/install_gcc.sh
@@ -8,8 +8,8 @@ set -e
 GCC_VERSION=15
 
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt update
-sudo apt install -y g++-$GCC_VERSION gcc-$GCC_VERSION gfortran-$GCC_VERSION
+sudo apt-get update
+sudo apt-get install -y g++-$GCC_VERSION gcc-$GCC_VERSION gfortran-$GCC_VERSION
 
 PRIORITY=$((${GCC_VERSION%%.*} * 10))
 sudo update-alternatives --remove-all gcc || true
