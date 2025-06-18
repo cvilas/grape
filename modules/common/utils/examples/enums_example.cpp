8aba9b95c8946c260d52836a4d68e4c8c6ebbb8c
Vilas Chitrakaran <cvilas@gmail.com>
2025-06-18 16:18:18 +0100

    fix: Reintroduces range formatting to support gcc-14

--- modules/common/utils/examples/enums_example.cpp
+++ modules/common/utils/examples/enums_example.cpp
@@ -6,6 +6,7 @@
 #include <print>
 
 #include "grape/utils/enums.h"
+#include "grape/utils/format_ranges.h"
 
 //=================================================================================================
 // Example program demonstrates usage of facilities for static reflection of enums
