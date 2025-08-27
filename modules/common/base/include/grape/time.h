ffdd257d8130df3ec1856890360ebcb276599fab
Vilas Chitrakaran <cvilas@gmail.com>
2025-08-27 19:31:32 +0100

    docs: Cleans up clock interface

--- modules/common/base/include/grape/time.h
+++ modules/common/base/include/grape/time.h
@@ -13,21 +13,23 @@ namespace grape {
 ///
 /// Avoids difference in internal representation across compilers
 struct SystemClock {
-  using Duration = std::chrono::nanoseconds;
+  using Duration = std::chrono::duration<std::int64_t, std::nano>;
   using TimePoint = std::chrono::time_point<std::chrono::system_clock, Duration>;
 
+  /// @return time now
   [[nodiscard]] static auto now() -> TimePoint {
     return std::chrono::time_point_cast<Duration>(std::chrono::system_clock::now());
   }
 
+  /// Returns a timepoint formatted as nanoseconds since Unix epoch
   [[nodiscard]] static constexpr auto toNanos(const SystemClock::TimePoint& tp) -> std::uint64_t {
-    const auto ns = std::chrono::duration_cast<std::chrono::nanoseconds>(tp.time_since_epoch());
+    const auto ns = std::chrono::duration_cast<Duration>(tp.time_since_epoch());
     return static_cast<std::uint64_t>(ns.count());
   }
 
+  /// Returns nanoseconds since Unix epoch formatted as a timepoint
   [[nodiscard]] static constexpr auto fromNanos(std::uint64_t nanos) -> SystemClock::TimePoint {
-    const auto dur =
-        std::chrono::duration_cast<SystemClock::Duration>(std::chrono::nanoseconds(nanos));
+    const auto dur = std::chrono::duration_cast<Duration>(std::chrono::nanoseconds(nanos));
     return SystemClock::TimePoint(dur);
   }
 };
