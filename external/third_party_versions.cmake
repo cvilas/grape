6d5f0313b6da4134e858f3fb0e4deccc85b87779
Vilas Chitrakaran <cvilas@gmail.com>
2025-10-04 06:28:14 +0100

    feat: Updates Catch2 and SDL
    (cherry picked from commit eb63786a32600a5516ffa9af5381f3dc08a9dba0)

--- external/third_party_versions.cmake
+++ external/third_party_versions.cmake
@@ -4,10 +4,10 @@
 
 # List of all third-party dependency versions
 
-set(CATCH2_VERSION_REQUIRED       3.10.0)
+set(CATCH2_VERSION_REQUIRED       3.11.0)
 set(BENCHMARK_VERSION_REQUIRED    1.9.4)
 set(FTXUI_VERSION_REQUIRED        6.1.9)
-set(SDL3_VERSION_REQUIRED         3.2.22)
+set(SDL3_VERSION_REQUIRED         3.2.24)
 set(ECAL_VERSION_REQUIRED         6.0.1)
 set(LZ4_VERSION_REQUIRED          1.10.0)
 set(NANOBIND_VERSION_REQUIRED     2.9.2)
