81f1216e9f67d3080bd39a7253820fdbf2eb001b
Vilas Chitrakaran <cvilas@gmail.com>
2025-12-29 12:11:56 +0000

    build: Upgrades Catch2
    (cherry picked from commit e2bcd1ee7480fd2d406bfe4f89fcc448f10ef409)

--- external/third_party_versions.cmake
+++ external/third_party_versions.cmake
@@ -4,7 +4,7 @@
 
 # List of all third-party dependency versions
 
-set(CATCH2_VERSION_REQUIRED       3.11.0)
+set(CATCH2_VERSION_REQUIRED       3.12.0)
 set(BENCHMARK_VERSION_REQUIRED    1.9.4)
 set(FTXUI_VERSION_REQUIRED        6.1.9)
 set(SDL3_VERSION_REQUIRED         3.2.28)
