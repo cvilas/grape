3273ae592abd2ee62c8c5a4897b38883d45c04d4
Vilas Chitrakaran <cvilas@gmail.com>
2026-01-15 07:58:29 +0000

    feat: Adds pi sense HAT

---
+++ modules/rpi/sense_hat/src/matrix_display.cpp
@@ -0,0 +1,7 @@
+//=================================================================================================
+// Copyright (C) 2026 GRAPE Contributors
+//=================================================================================================
+
+#include "grape/rpi/matrix_display.h"
+
+namespace grape::rpi::sense_hat {}  // namespace grape::rpi::sense_hat
