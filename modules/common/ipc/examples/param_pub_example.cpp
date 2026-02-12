cf8ddbfe0ceed3022e58e4989f2c906e68d8a19f
Vilas Chitrakaran <cvilas@gmail.com>
2026-02-12 06:45:06 +0000

    fix(ipc): Introduce a flag for clean exit
    (cherry picked from commit 3ec2f61d0f3282eed9074a86daa7554346b7f6de)

--- modules/common/ipc/examples/param_pub_example.cpp
+++ modules/common/ipc/examples/param_pub_example.cpp
@@ -2,6 +2,7 @@
 // Copyright (C) 2026 GRAPE Contributors
 //=================================================================================================
 
+#include <csignal>
 #include <cstddef>
 #include <print>
 #include <thread>
@@ -34,20 +35,33 @@ private:
   grape::ipc::Publisher<Topic> publisher_;
 };
 
+namespace {
+
+//-------------------------------------------------------------------------------------------------
+std::atomic_flag s_exit = false;  // NOLINT(cppcoreguidelines-avoid-non-const-global-variables)
+
+void onSignal(int /*signum*/) {
+  s_exit.test_and_set();
+  s_exit.notify_one();
+}
+
+}  // namespace
+
 //=================================================================================================
 // Demonstrates a const parameter IPC publisher. See sub_example.cpp for matching subscriber.
 auto main() -> int {
   try {
+    std::ignore = signal(SIGINT, onSignal);
+    std::ignore = signal(SIGTERM, onSignal);
+
     grape::ipc::init(grape::ipc::Config{});
 
     const auto params = std::string("key=value");
     const auto topic = grape::ipc::ex::ExampleTopicAttributes{};
     auto server = ParameterPublisher(topic, params);
 
-    constexpr auto SLEEP_TIME = std::chrono::milliseconds(500);
-    while (grape::ipc::ok()) {
-      std::this_thread::sleep_for(SLEEP_TIME);
-    }
+    std::println("Press ctrl-c to exit");
+    s_exit.wait(false);
     return EXIT_SUCCESS;
   } catch (...) {
     grape::Exception::print();
