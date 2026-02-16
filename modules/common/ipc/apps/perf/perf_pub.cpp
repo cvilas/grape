cccaf0c3b1af1a614b75f08dd2dcb0e2e6d9f1bb
Vilas Chitrakaran <cvilas@gmail.com>
2026-02-16 13:16:27 +0000

    feat(ipc): Introduces --interval option to performance tester
    (cherry picked from commit c23fb65b9b034e77ec26cddb72813fb510b93365)

--- modules/common/ipc/apps/perf/perf_pub.cpp
+++ modules/common/ipc/apps/perf/perf_pub.cpp
@@ -29,30 +29,38 @@
 auto main(int argc, const char* argv[]) -> int {
   try {
     static constexpr auto DEFAULT_PAYLOAD_SIZE = 8U;
+    static constexpr auto DEFAULT_INTERVAL = 0.0;
     static constexpr std::byte DEFAULT_PAYLOAD_FILL{ 0x01 };
 
     const auto args =
         grape::conio::ProgramDescription(
             "Publishing end of IPC performance measurement application pair")
             .declareOption<std::size_t>("size", "payload size in bytes", DEFAULT_PAYLOAD_SIZE)
+            .declareOption<double>("interval", "publish interval in seconds", DEFAULT_INTERVAL)
             .parse(argc, argv);
 
     const auto payload_size = args.getOption<std::size_t>("size");
     const auto payload = std::vector<std::byte>(payload_size, DEFAULT_PAYLOAD_FILL);
     std::println("Payload size: {} bytes", payload_size);
 
+    const auto interval_sec = std::chrono::duration<double>(args.getOption<double>("interval"));
+    const auto interval =
+        std::chrono::duration_cast<std::chrono::steady_clock::duration>(interval_sec);
+    std::println("Publish interval: {} s", interval_sec.count());
+
     const auto config = grape::ipc::Config{ .scope = grape::ipc::Config::Scope::Network };
     grape::ipc::init(config);
 
     auto pub = grape::ipc::RawPublisher(grape::ipc::ex::perf::TOPIC);
+    auto next_tick = std::chrono::steady_clock::now();
     std::println("Press CTRL-C to quit");
     while (grape::ipc::ok()) {
       const auto pub_result = pub.publish(payload);
       if (not pub_result) {
-        std::println("Publish failed: {}", toString(pub_result.error()));
+        std::println("ERROR: {}", toString(pub_result.error()));
       }
-      // force a context switch to mimic reality of cache invalidation, etc
-      std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+      next_tick += interval;
+      std::this_thread::sleep_until(next_tick);
     }
     return EXIT_SUCCESS;
   } catch (...) {
