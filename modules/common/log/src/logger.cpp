dc0c54542323101cbd206c92bb39e7446c4fd7dd
Vilas Chitrakaran <cvilas@gmail.com>
2026-04-16 04:03:10 +0100

    refactor(log): Minor performance improvements
    (cherry picked from commit aea3ef1d4390da0687430c8ed4ed2abc1b6a8cfb)

--- modules/common/log/src/logger.cpp
+++ modules/common/log/src/logger.cpp
@@ -4,7 +4,6 @@
 
 #include "grape/log/logger.h"
 
-#include <cstring>  // for memcpy
 #include <thread>
 
 #include "grape/exception.h"
@@ -31,20 +30,6 @@ Logger::~Logger() {
 }
 
 //-------------------------------------------------------------------------------------------------
-void Logger::log(const Record& record) {
-  if (not canLog(record.severity)) {
-    return;
-  }
-  const auto writer = [&record](std::span<std::byte> frame) -> void {
-    assert(sizeof(record) == frame.size_bytes());
-    std::memcpy(frame.data(), &record, sizeof(Record));
-  };
-  if (not queue_.visitToWrite(writer)) {
-    missed_logs_.fetch_add(1, std::memory_order_relaxed);
-  }
-}
-
-//-------------------------------------------------------------------------------------------------
 void Logger::sinkLoop(const std::stop_token& st) noexcept {
   while (not st.stop_requested()) {
     std::this_thread::sleep_for(config_.flush_period);
@@ -59,14 +44,8 @@ void Logger::flush() noexcept {
     auto record_reader = [this](std::span<const std::byte> frame) -> void {
       assert(sizeof(Record) == frame.size_bytes());
       if (config_.sink != nullptr) {
-        /// @note: Sets logger name in the record here instead of in the logging hot path. This
-        /// requires casting away const qualifier on the record frame. Should be safe to do since
-        /// data types are fixed size and only this location reads the queue
         // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-        const auto* const const_record = reinterpret_cast<const Record*>(frame.data());
-        // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
-        auto* record = const_cast<Record*>(const_record);
-        record->logger_name.append(config_.logger_name.c_str());
+        const auto* const record = reinterpret_cast<const Record*>(frame.data());
         config_.sink->write(*record);
       }
     };
@@ -84,7 +63,7 @@ void Logger::flush() noexcept {
         config_.sink->write({
             .timestamp{ WallClock::now() },                      //
             .location{ std::source_location::current() },        //
-            .logger_name{ config_.logger_name.c_str() },         //
+            .logger_name{ config_.logger_name },                 //
             .message{ "{} records missed", delta_missed_logs },  //
             .severity = Severity::Warn,
         });
