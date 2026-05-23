f20f5493fe43443e855c1728e10b9dbf6f6f5887
Vilas Chitrakaran <cvilas@gmail.com>
2026-05-23 08:51:09 +0100

    fix(ipc): Removes a minor duplication

--- modules/common/ipc/src/discovery.cpp
+++ modules/common/ipc/src/discovery.cpp
@@ -6,8 +6,19 @@
 
 #include <ecal/registration.h>
 
+namespace {
+//-------------------------------------------------------------------------------------------------
+auto toEndpointInfo(const eCAL::STopicId& id, const eCAL::SDataTypeInformation& type_info)
+    -> grape::ipc::EndpointInfo {
+  return { .entity_id = { .host = id.topic_id.host_name, .id = id.topic_id.entity_id },
+           .type_name = type_info.name,
+           .encoding = type_info.encoding };
+}
+}  // namespace
+
 namespace grape::ipc {
 
+//-------------------------------------------------------------------------------------------------
 auto discover() -> std::unordered_map<TopicName, Endpoints> {
   auto topics = std::unordered_map<TopicName, Endpoints>{};
 
@@ -16,10 +27,7 @@ auto discover() -> std::unordered_map<TopicName, Endpoints> {
   for (const auto& id : pub_ids) {
     auto type_info = eCAL::SDataTypeInformation{};
     std::ignore = eCAL::Registration::GetPublisherInfo(id, type_info);
-    topics[id.topic_name].publishers.push_back(
-        { .entity_id = { .host = id.topic_id.host_name, .id = id.topic_id.entity_id },
-          .type_name = type_info.name,
-          .encoding = type_info.encoding });
+    topics[id.topic_name].publishers.push_back(toEndpointInfo(id, type_info));
   }
 
   auto sub_ids = std::set<eCAL::STopicId>{};
@@ -27,10 +35,7 @@ auto discover() -> std::unordered_map<TopicName, Endpoints> {
   for (const auto& id : sub_ids) {
     auto type_info = eCAL::SDataTypeInformation{};
     std::ignore = eCAL::Registration::GetSubscriberInfo(id, type_info);
-    topics[id.topic_name].subscribers.push_back(
-        { .entity_id = { .host = id.topic_id.host_name, .id = id.topic_id.entity_id },
-          .type_name = type_info.name,
-          .encoding = type_info.encoding });
+    topics[id.topic_name].subscribers.push_back(toEndpointInfo(id, type_info));
   }
   return topics;
 }
